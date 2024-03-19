3ef7a6406e6407326680f3a98cf8acd9296acf90
prabinnnn <prabinbhandari120@gmail.com>
2024-03-19 15:16:16 +0545

    add registrate

--- play/registration.js
+++ play/registration.js
@@ -1,8 +1,11 @@
-const sendData = () => {
+const sendData = async () => {
   try {
     const form = document.getElementById("registration");
     const formData = new FormData(form);
-    axios.post("http://localhost:8000/api/v1/users/registration");
+    const result = await axios.post(
+      "http://localhost:8000/api/v1/users/registration",
+      formData
+    );
   } catch (e) {
     console.log(e);
   }
